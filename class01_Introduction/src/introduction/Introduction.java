eb5c34b4da61a9a735a37e5e369b8f415a59a5f9
addri <addri@LAPTOP-SITBJLS4.lan>
2022-09-18 12:19:29 -0400

    "first commit"

---
+++ class01_Introduction/src/introduction/Introduction.java
@@ -0,0 +1,44 @@
+package introduction;
+
+public class Introduction {
+	
+	public static void main(String[] args) {
+
+		
+		System.out.println("New York, USA");	//Statement of programming language
+				//System-System Class
+				//out-class object
+				//println() Method call 			                                                                                                                                                                                 as, Texas"); // println means next line
+
+			//// row-5 open CurlyBrace'{' to row-12 closing CurlyBrace '}' is Method Body
+
+		// row-3 open CurlyBrace'{' to row-14 closing CurlyBrace '}' is Class Body
+
+		// () Parenthesis
+		// {} Curly Braces
+		// [] Square Braces
+		//  ; Semi Colon
+		//  " Double Quote
+		//  ' Single Quote
+		//  * Asterisk
+		//  & Ampersand
+		//  ^ Tuple
+		//  % Modulus Operator
+		//How to create a project
+		//How to create a class
+		//what is main method
+
+		//Data Type Default Value (for fields)
+		//byte 		0
+		//short 	0
+		//int 		0
+		//long 		0L
+		//float 	0.0f
+		//double 	0.0d
+		//char 		'\u0000'
+		//String	null
+		//
+
+	}//main
+
+}//class
